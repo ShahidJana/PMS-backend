b847f0164f6e02a79b7f2f8054ecfd4cc6dd094c
shahidjana <shahidjana>
2026-01-05 17:28:36 +0500

    first commit

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-XSRF-TOKEN'],
   credentials: true
 }));
 app.use(express.json());
